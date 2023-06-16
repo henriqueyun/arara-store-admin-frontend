4acdcb564340a7b2690b0acd7cd9b80b477c65a5
henriqueyun <henriqueyun@gmail.com>
2023-06-16 17:08:53 -0300

    feat: palette primary #010101

--- src/theme/Theme.js
+++ src/theme/Theme.js
@@ -1,24 +1,30 @@
-import { createTheme } from "@mui/material/styles";
-import { light } from "@mui/material/styles/createPalette";
-import { grey } from "@mui/material/colors";
+/* eslint-disable prettier/prettier */
+import { createTheme } from '@mui/material/styles';
+import { light } from '@mui/material/styles/createPalette';
+import { grey } from '@mui/material/colors';
 
 const theme = createTheme({
     mode: light,
+    palette: {
+        primary: {
+            main: "#010101"
+        }
+    },
     components: {
         MuiDrawer: {
             styleOverrides: {
                 paper: {
                     backgroundColor: grey[900],
-                    color: "#FFFFFF",
+                    color: '#FFFFFF',
                     flexShrink: 0,
                 },
                 root: {
                     flexShrink: 0,
                     width: 240,
-                }
-            }
-        }
-    }
-})
+                },
+            },
+        },
+    },
+});
 
 export default theme;
